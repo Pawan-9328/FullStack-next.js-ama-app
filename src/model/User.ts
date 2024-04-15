bf3429bc99a495a320573b5a63b93ab66c945a84
Pawan Gola <pawangola908@gmail.com>
2024-04-16 04:48:17 +0530

    Basic setup completed and User modles Add

---
+++ src/model/User.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verfiyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email:{
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    //email validation used RegEx
+    match: [/ .+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verfiyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  message: [MessageSchema]
+
+}
+);
+// in next.js diffrent way for export because next.js run on edge case 
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
+
